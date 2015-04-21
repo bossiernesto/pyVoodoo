b5949ed503d0295860d99e79463a74913fd7b02e
bossiernesto <bossi.ernestog@gmail.com>
2015-04-20 21:16:51 -0300

    Fix tests

--- test/test_pydisasemble.py
+++ test/test_pydisasemble.py
@@ -5,7 +5,7 @@ class TestDissasemble(unittest.TestCase):
 
     def test_compile_simple_pyfile(self):
         raw_code = b'd\x00\x00d\x01\x00l\x00\x00Z\x00\x00Gd\x02\x00d\x03\x00\x84\x00\x00d\x03\x00e\x01\x00\x83\x03\x00Z\x02\x00e\x02\x00\x83\x00\x00Z\x03\x00e\x03\x00j\x04\x00\x83\x00\x00\x01e\x05\x00e\x03\x00j\x03\x00\x83\x01\x00\x01d\x01\x00S'
-        code =PythonParser()._parse_from_py('fixture/a.py', False)
+        code =PythonParser()._parse_from_py('test/fixture/a.py', False)
         self.assertEqual(raw_code, code.co_code)
         self.assertEqual(1, code.co_firstlineno)
         self.assertEqual(64, code.co_flags)
